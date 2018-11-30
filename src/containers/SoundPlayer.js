69a95a297845ac3af01e88361131cc1141335f90
Markus Kullberg <kullberg.markus@gmail.com>
2018-11-30 09:57:21 +0200

    WIP

--- src/containers/SoundPlayer.js
+++ src/containers/SoundPlayer.js
@@ -1,15 +1,25 @@
 import { Component } from 'react';
 import Sound from 'react-native-sound';
 import { connect } from 'react-redux';
+import { SOUND_ID_ONE, SOUND_ID_TWO, SOUND_ID_THREE, SOUND_ID_THIRTY, SOUND_ID_TICK } from '../constants';
 import { removeSound } from '../actions';
 
-const importSound = path => {
-  return require(path);
-}
+import one from '../assets/sounds/ut2003/evil/one.wav';
+import two from '../assets/sounds/ut2003/evil/two.wav';
+import three from '../assets/sounds/ut2003/evil/three.wav';
+import thirty from '../assets/sounds/ut2003/evil/30sec.wav';
+import tick from '../assets/sounds/tick.wav';
+
+const soundsMapping = {
+  [SOUND_ID_ONE]: one,
+  [SOUND_ID_TWO]: two,
+  [SOUND_ID_THREE]: three,
+  [SOUND_ID_THIRTY]: thirty,
+  [SOUND_ID_TICK]: tick,
+};
 
 class SoundPlayer extends Component {
   componentDidMount() {
-
     this.initializeSounds();
   }
 
@@ -24,13 +34,11 @@ class SoundPlayer extends Component {
     this.releaseAllSounds();
   }
 
-  async initializeSounds() {
+  initializeSounds() {
     Sound.setCategory('Playback');
     this.sounds = {};
-    const { basePath, announcerPath } = this.props;
-    for (const [key, soundFile] of Object.entries(this.props.sounds.announcerSounds)) {
-      importSound(`../${basePath}${announcerPath}${soundFile}`)
-      .then(sound => this.loadSound(sound, key))
+    for (const [key, soundFile] of Object.entries(soundsMapping)) {
+      this.loadSound(soundFile, key);
     }
   }
 
@@ -67,10 +75,7 @@ class SoundPlayer extends Component {
 
 export default connect(
   state => ({
-    announcerPath: state.sound.announcerPath,
-    basePath: state.sound.basePath,
     pendingSounds: state.sound.soundQueue,
-    sounds: state.sound.sounds,
   }),
   {
     removeSound,
